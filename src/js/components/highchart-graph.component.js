061d58e8558142227a4d863c630bf168ade729fc
Dani Nebot <daniign@gmail.com>
2016-06-25 01:20:56 +0200

    Add option to enable/disable graph legend

--- src/js/components/highchart-graph.component.js
+++ src/js/components/highchart-graph.component.js
@@ -2,16 +2,21 @@ class HighchartGraphCtrl {
   constructor() {
     let Highcharts = require('highcharts');
 
-    // this.data provided by bindings
+    // this.data, this.legend provided by bindings
     this.data.chart = this.data.chart || {};
     this.data.chart.renderTo = 'graph';
+
+    this.data.legend = this.data.legend || {};
+    this.data.legend.enabled = this.legend;
+
     let chart = new Highcharts.Chart(this.data);
   }
 }
 
 let HighchartGraph = {
   bindings: {
-    data: '<'
+    data: '<',
+    legend: '<'
   },
   controller: HighchartGraphCtrl,
   templateUrl: 'components/highchart-graph.html'
