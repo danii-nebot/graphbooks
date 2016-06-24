e88362448224b027bf2139c5409b46e026ada278
Dani Nebot <daniign@gmail.com>
2016-06-24 21:41:32 +0200

    Remove html5Mode in production environment

--- src/js/config/app.config.js
+++ src/js/config/app.config.js
@@ -1,7 +1,9 @@
 function AppConfig($httpProvider, $stateProvider, $locationProvider, $urlRouterProvider) {
   'ngInject';
 
-  $locationProvider.html5Mode(true);
+  // remove html5mode for now, until we can modify the server htaccess
+  // per: http://stackoverflow.com/questions/16569841/reloading-the-page-gives-wrong-get-request-with-angularjs-html5-mode
+  // $locationProvider.html5Mode(true);
 
   $stateProvider
   .state('app', {
@@ -10,7 +12,6 @@ function AppConfig($httpProvider, $stateProvider, $locationProvider, $urlRouterP
   });
 
   $urlRouterProvider.otherwise('/');
-
 }
 
 export default AppConfig;
