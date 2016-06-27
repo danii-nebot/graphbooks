1fa353bb34d57ae7b0232e6bbb63d42c2b924af5
Dani Nebot <daniign@gmail.com>
2016-06-27 14:12:40 +0200

    Add search form in Home page

--- src/js/home/home.controller.js
+++ src/js/home/home.controller.js
@@ -3,9 +3,17 @@ export default class HomeCtrl {
     'ngInject';
     this._$state = $state;
     this.appName = AppConstants.appName;
+    this.formData = {};
   }
 
   goto(newState) {
     this._$state.go('app.' + newState);
+  }
+
+  submit() {
+    if(this.formData.seriesName) {
+      // TODO: get slug from name
+      this._$state.go('app.graph', {slug:'placeholder'});
+    }
   }
 }
