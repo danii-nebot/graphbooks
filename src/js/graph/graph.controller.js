a1398739f27cc1a413ffa439c06a6b930c96136c
Dani Nebot <daniign@gmail.com>
2016-06-27 14:21:16 +0200

    Remove error handling from Highchart graph Component

--- src/js/graph/graph.controller.js
+++ src/js/graph/graph.controller.js
@@ -3,7 +3,12 @@ export default class GraphCtrl {
     'ngInject';
 
     GraphData.get($stateParams.slug).then(
-      (data) => this.data = data,
+      (data) => {
+        this.data = data;
+        if(!this.data) {
+          this.error = "Data not available";
+        }
+      },
       (err) => this.error = err
     )
 
