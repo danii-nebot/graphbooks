066f46426ad78a109dfacf06267a535b9ff388cc
Dani Nebot <daniign@gmail.com>
2016-06-27 14:14:59 +0200

    Add error handling for graph page and component

--- src/js/components/highchart-graph.component.js
+++ src/js/components/highchart-graph.component.js
@@ -1,25 +1,27 @@
 class HighchartGraphCtrl {
   constructor() {
-    let Highcharts = require('highcharts');
+    if(this.data) {
+      let Highcharts = require('highcharts');
 
-    // this.data, this.legend provided by bindings
-    this.data.chart = this.data.chart || {};
-    this.data.chart.renderTo = 'graph';
+      // this.data, this.legend provided by bindings
+      this.data.chart = this.data.chart || {};
+      this.data.chart.renderTo = 'graph';
 
-    this.data.legend = this.data.legend || {};
-    this.data.legend.enabled = this.legend;
+      this.data.legend = this.data.legend || {};
+      this.data.legend.enabled = this.legend;
 
-    // TODO: should this be here?
-    this.data.tooltip = {
-      formatter: function() {
-        return '<b>'+ this.point.data.title +'</b><br/>'+
-               'Year: ' + this.point.x +'<br/>'+
-               'Rating:' + this.point.y + '<br/>'+
-               'Votes: ' + this.point.data.numVotes + '<br/>';
+      // TODO: should this be here?
+      this.data.tooltip = {
+        formatter: function() {
+          return '<b>'+ this.point.data.title +'</b><br/>'+
+          'Year: ' + this.point.x +'<br/>'+
+          'Rating:' + this.point.y + '<br/>'+
+          'Votes: ' + this.point.data.numVotes + '<br/>';
+        }
       }
-    }
 
-    let chart = new Highcharts.Chart(this.data);
+      let chart = new Highcharts.Chart(this.data);
+    }
   }
 }
 
