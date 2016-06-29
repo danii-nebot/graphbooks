63b692635abbaa3ea93b18aa609c90ca5a9d912a
Dani Nebot <daniign@gmail.com>
2016-06-29 21:11:41 +0200

    Add search autocomplete from data list functionality
    Use mock data for now

--- src/js/components/search-form.component.js
+++ src/js/components/search-form.component.js
@@ -4,13 +4,39 @@ class SearchFormCtrl {
 
     this._$state = $state;
     this.formData = {};
+
+    this.series = [
+      { title: 'A Song of Ice and Fire', author: 'George RR Martin', keywords: 'Game of Thrones', slug:'a-song-of-ice-and-fire'},
+      { title: 'Foundation', author: 'Isaac Asimov', keywords: '', slug:'foundation'}
+    ];
+  };
+
+  reset() {
+    this.formData.slug = '';
+    this.autocompleteActive = true;
+  }
+
+  select(series) {
+    this.formData.seriesName = series.title;
+    this.formData.slug = series.slug;
+    this.autocompleteActive = false;
   }
 
   submit() {
     if(this.formData.seriesName) {
-      // TODO: get slug from name
-      this._$state.go('app.graph', {slug:'placeholder'});
+      let slug = this.formData.slug || this.slugify(this.formData.seriesName);
+      this._$state.go('app.graph', {slug: slug});
     }
+  };
+
+  slugify(text)
+  {
+    return text.toString().toLowerCase()
+      .replace(/\s+/g, '-')           // Replace spaces with -
+      .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
+      .replace(/\-\-+/g, '-')         // Replace multiple - with single -
+      .replace(/^-+/, '')             // Trim - from start of text
+      .replace(/-+$/, '');            // Trim - from end of text
   }
 
 };
