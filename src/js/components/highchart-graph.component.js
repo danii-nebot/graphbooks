1abb6922b0653d374130ea4537454a80feb792e0
Dani Nebot <daniign@gmail.com>
2016-07-05 11:54:47 +0200

    Refactor graph options data wrangling code to graph component
    highchart-graph component now is completely project agnostic and does
    not depend on GraphBooks data schema

--- src/js/components/highchart-graph.component.js
+++ src/js/components/highchart-graph.component.js
@@ -3,7 +3,7 @@ class HighchartGraphCtrl {
     if(this.data) {
       let Highcharts = require('highcharts');
 
-      let graphOptions = this.createGraphOptions(this.data);
+      let graphOptions = this.data;
 
       // this.data, this.legend provided by bindings
       graphOptions.chart = graphOptions.chart || {};
@@ -12,64 +12,9 @@ class HighchartGraphCtrl {
       graphOptions.legend = graphOptions.legend || {};
       graphOptions.legend.enabled = this.legend;
 
-      // TODO: should this be here?
-      graphOptions.tooltip = {
-        formatter: function() {
-          return '<b>'+ this.point.data.title +'</b><br/>'+
-          'Year: ' + this.point.x +'<br/>'+
-          'Rating:' + this.point.y + '<br/>'+
-          'Votes: ' + this.point.data.numVotes + '<br/>';
-        }
-      }
-
       let chart = new Highcharts.Chart(graphOptions);
     }
   }
-
-  createGraphOptions(data) {
-    let options = {
-      xAxis: {
-        min: data.xAxisMin,
-        max: data.xAxisMax
-      },
-      yAxis: {
-        min: data.yAxisMin,
-        max: 5.00,
-        title: {
-          text: 'Ratings'
-        }
-      },
-      title: {
-        text: data.title
-      },
-      series: [
-        {
-          type: 'line',
-          name: 'Regression Line for Series',
-          data: data.regressionData,
-          marker: {
-            enabled: false
-          },
-          states: {
-            hover: {
-              lineWidth: 0
-            }
-          },
-          enableMouseTracking: false
-        },
-        {
-          type: 'scatter',
-          name: 'Books in Series',
-          data: data.data,
-          marker: {
-            radius: 4
-          }
-        }
-      ]
-    }
-    return options;
-  }
-
 };
 
 let HighchartGraph = {
--- src/js/graph/graph.controller.js
+++ src/js/graph/graph.controller.js
@@ -2,10 +2,65 @@ export default class GraphCtrl {
   constructor(seriesData, $rootScope, $state) {
     'ngInject';
 
-    // Update the title of this page
     if(seriesData) {
       this.series = seriesData;
-      $rootScope.setPageTitle('Graph ' + this.series.title);
+      this.graphData = this.createGraphOptions(seriesData.graph, seriesData.title);
+      // Update the title of this page
+      $rootScope.setPageTitle(`GraphBooks - ${this.series.title}`);
     }
   }
+
+  createGraphOptions(data, title) {
+    let options = {
+      xAxis: {
+        min: data.xAxisMin,
+        max: data.xAxisMax
+      },
+      yAxis: {
+        min: data.yAxisMin,
+        max: 5.00,
+        title: {
+          text: 'Ratings'
+        }
+      },
+      title: {
+        text: title
+      },
+      series: [
+        {
+          type: 'line',
+          name: 'Regression Line for Series',
+          data: data.regressionData,
+          marker: {
+            enabled: false
+          },
+          states: {
+            hover: {
+              lineWidth: 0
+            }
+          },
+          enableMouseTracking: false
+        },
+        {
+          type: 'scatter',
+          name: 'Books in Series',
+          data: data.data,
+          marker: {
+            radius: 4
+          }
+        }
+      ],
+      tooltip: {
+        formatter: function() {
+          return `<b>${this.point.data.title}</b><br/>
+          Year: ${this.point.x}<br/>
+          Rating: ${this.point.y}<br/>
+          Votes: ${this.point.data.numVotes}<br/>`;
+        }
+      }
+    }
+
+    return options;
+  }
+
 }
